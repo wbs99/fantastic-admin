a7768a0c12a81c941c14972ae6b6cd14c56768e2
wbs <1134954328@qq.com>
2023-02-25 16:45:45 +0800

    修改首页和全局配置

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,54 @@
 import type { Settings } from '#/global'
 
-const globalSettings: Settings.all = {}
+const globalSettings: Settings.all = {
+  "app": {
+    "enablePermission": true,
+    "enableDynamicTitle": true,
+    "colorScheme": "light",
+    "elementSize": "default",
+    "iconifyOfflineUse": false,
+    "enableProgress": true,
+    "routeBaseOn": "frontend"
+  },
+  "layout": {
+    "enableMobileAdaptation": true
+  },
+  "menu": {
+    "enableSubMenuCollapseButton": true,
+    "enableHotkeys": true,
+    "baseOn": "frontend",
+    "menuMode": "single",
+    "switchMainMenuAndPageJump": false,
+    "subMenuUniqueOpened": true,
+    "subMenuCollapse": false
+  },
+  "topbar": {
+    "mode": "fixed"
+  },
+  "toolbar": {
+    "enableFullscreen": true,
+    "enablePageReload": true,
+    "enableColorScheme": true,
+    "enableAppSetting": true
+  },
+  "home": {
+    "enable": true,
+    "title": "主页"
+  },
+  "breadcrumb": {
+    "enable": true
+  },
+  "navSearch": {
+    "enable": true,
+    "enableHotkeys": true
+  },
+  "copyright": {
+    "enable": true,
+    "dates": "2020-2023",
+    "company": "Fantastic-admin",
+    "website": "https://hooray.gitee.io/fantastic-admin/",
+    "beian": ""
+  }
+}
 
 export default globalSettings
